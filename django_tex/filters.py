25aef7b7c715e2f6215c9884bb9718eb11521e58
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2019-08-09 17:32:30 -0300

    add filter capitalize

--- django_tex/filters.py
+++ django_tex/filters.py
@@ -1,24 +1,32 @@
 from django.utils.formats import localize_input
 from django.template.defaultfilters import register
 
+
 def do_linebreaks(value):
     return value.replace('\n', '\\\\\n')
 
+
 def do_latex_escape(value):
     return (value
-        .replace('&', '\\&')
-        .replace('$', '\\$')
-        .replace('%', '\\%')
-        .replace('#', '\\#')
-        .replace('_', '\\_')
-        .replace('{', '\\{')
-        .replace('}', '\\}')
-        )
+            .replace('&', '\\&')
+            .replace('$', '\\$')
+            .replace('%', '\\%')
+            .replace('#', '\\#')
+            .replace('_', '\\_')
+            .replace('{', '\\{')
+            .replace('}', '\\}')
+            )
+
+
+def capitalize(value):
+    return (value.capitalize())
+
 
 tex_specific_filters = {
     'localize': localize_input,
     'linebreaks': do_linebreaks,
     'latex_escape': do_latex_escape,
+    'capitalize': capitalize,
 }
 
 FILTERS = register.filters.copy()
